6a79e18757b2b75454c556dd03fa03a6a76eb300
刘祥超 <iwind.liu@gmail.com>
2019-03-23 13:24:37 +0800

    [proxy]纠正统计中"闽"、或者没"省"的问题

--- tealogs/access_log.go
+++ tealogs/access_log.go
@@ -415,6 +415,14 @@ func (this *AccessLog) parseGeoIP() {
 	if len(record.Country.Names) > 0 {
 		name, found := record.Country.Names["zh-CN"]
 		if found {
+			switch name {
+			case "台湾":
+				name = "中国台湾"
+			case "香港":
+				name = "中国香港"
+			case "澳门":
+				name = "中国澳门"
+			}
 			this.Extend.Geo.Region = name
 		}
 	}
@@ -422,6 +430,36 @@ func (this *AccessLog) parseGeoIP() {
 	if len(record.Subdivisions) > 0 && len(record.Subdivisions[0].Names) > 0 {
 		name, found := record.Subdivisions[0].Names["zh-CN"]
 		if found {
+			switch name {
+			case "台湾":
+				name = "中国台湾"
+			case "香港":
+				name = "中国香港"
+			case "澳门":
+				name = "中国澳门"
+			case "闽":
+				name = "福建省"
+			case "河南":
+				name = "河南省"
+			case "重庆":
+				name = "重庆市"
+			case "安徽":
+				name = "安徽省"
+			case "上海":
+				name = "上海市"
+			case "辽宁":
+				name = "辽宁省"
+			case "贵州":
+				name = "贵州省"
+			case "湖南":
+				name = "湖南省"
+			case "海南":
+				name = "海南省"
+			case "江西":
+				name = "江西省"
+			case "广东":
+				name = "广东省"
+			}
 			this.Extend.Geo.State = name
 		}
 	}
