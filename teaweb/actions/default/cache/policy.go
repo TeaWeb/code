a472057d2542844d88d33482a679d5cd9234fe6c
刘祥超 <iwind.liu@gmail.com>
2019-04-14 18:26:45 +0800

    [cache]列出当前正在使用缓存策略的项目

--- teaweb/actions/default/cache/policy.go
+++ teaweb/actions/default/cache/policy.go
@@ -2,8 +2,10 @@ package cache
 
 import (
 	"github.com/TeaWeb/code/teacache"
+	"github.com/TeaWeb/code/teaconfigs"
 	"github.com/TeaWeb/code/teaconfigs/shared"
 	"github.com/iwind/TeaGo/actions"
+	"github.com/iwind/TeaGo/maps"
 )
 
 type PolicyAction actions.Action
@@ -21,6 +23,35 @@ func (this *PolicyAction) Run(params struct {
 
 	// 类型
 	this.Data["type"] = teacache.FindType(policy.Type)
+
+	// 正在使用此缓存策略的项目
+	configItems := []maps.Map{}
+	serverList, _ := teaconfigs.SharedServerList()
+	if serverList != nil {
+		for _, server := range serverList.FindAllServers() {
+
+			if server.CachePolicy == policy.Filename {
+				configItems = append(configItems, maps.Map{
+					"type":   "server",
+					"server": server.Description,
+					"link":   "/proxy/cache?serverId=" + server.Id,
+				})
+			}
+
+			for _, location := range server.Locations {
+				if location.CachePolicy == policy.Filename {
+					configItems = append(configItems, maps.Map{
+						"type":     "location",
+						"server":   server.Description,
+						"location": location.Pattern,
+						"link":     "/proxy/locations/cache?serverId=" + server.Id + "&locationId=" + location.Id,
+					})
+				}
+			}
+		}
+	}
+
+	this.Data["configItems"] = configItems
 
 	this.Show()
 }
