50cf740a1dfe171c52eed7fa9b8240765899d179
刘祥超 <iwind.liu@gmail.com>
2019-05-11 09:41:24 +0800

    [waf]将被阻止的请求响应从405改成403

--- teawaf/actions/action_block.go
+++ teawaf/actions/action_block.go
@@ -7,7 +7,7 @@ type BlockAction struct {
 
 func (this *BlockAction) Perform(writer http.ResponseWriter) (allow bool) {
 	if writer != nil {
-		writer.WriteHeader(http.StatusMethodNotAllowed)
+		writer.WriteHeader(http.StatusForbidden)
 		writer.Write([]byte("The request is blocked by TeaWAF"))
 	}
 	return false
