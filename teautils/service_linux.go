bbb47fe0f37dee6a93983ba8f694a76c174f2d12
刘祥超 <iwind.liu@gmail.com>
2019-07-28 20:09:26 +0800

    实现systemd service的管理

--- teautils/service_linux.go
+++ teautils/service_linux.go
@@ -7,14 +7,75 @@ import (
 	"github.com/iwind/TeaGo/Tea"
 	"github.com/iwind/TeaGo/files"
 	"io/ioutil"
+	"os"
 	"os/exec"
 	"regexp"
 )
 
-var serviceFile = "/etc/init.d/teaweb"
+var systemdServiceFile = "/etc/systemd/system/teaweb.service"
+var initServiceFile = "/etc/init.d/teaweb"
 
 // 安装服务
 func (this *ServiceManager) Install(exePath string, args []string) error {
+	if os.Getgid() != 0 {
+		return errors.New("only root users can install the service")
+	}
+
+	systemd, err := exec.LookPath("systemctl")
+	if err != nil {
+		return this.installInitService(exePath, args)
+	}
+
+	return this.installSystemdService(systemd, exePath, args)
+}
+
+// 启动服务
+func (this *ServiceManager) Start() error {
+	if os.Getgid() != 0 {
+		return errors.New("only root users can start the service")
+	}
+
+	if files.NewFile(systemdServiceFile).Exists() {
+		systemd, err := exec.LookPath("systemctl")
+		if err != nil {
+			return err
+		}
+
+		return exec.Command(systemd, "start", "teaweb.service").Start()
+	}
+	return exec.Command("service", "teaweb", "start").Start()
+}
+
+// 删除服务
+func (this *ServiceManager) Uninstall() error {
+	if os.Getgid() != 0 {
+		return errors.New("only root users can uninstall the service")
+	}
+
+	if files.NewFile(systemdServiceFile).Exists() {
+		systemd, err := exec.LookPath("systemctl")
+		if err != nil {
+			return err
+		}
+
+		// disable service
+		exec.Command(systemd, "disable", "teaweb.service").Start()
+
+		// reload
+		exec.Command(systemd, "daemon-reload")
+
+		return files.NewFile(systemdServiceFile).Delete()
+	}
+
+	f := files.NewFile(initServiceFile)
+	if f.Exists() {
+		return f.Delete()
+	}
+	return nil
+}
+
+// install init service
+func (this *ServiceManager) installInitService(exePath string, args []string) error {
 	scriptFile := Tea.Root + "/scripts/teaweb"
 	if !files.NewFile(scriptFile).Exists() {
 		return errors.New("'scripts/teaweb' file not exists")
@@ -26,7 +87,7 @@ func (this *ServiceManager) Install(exePath string, args []string) error {
 	}
 
 	data = regexp.MustCompile("INSTALL_DIR=.+").ReplaceAll(data, []byte("INSTALL_DIR="+Tea.Root))
-	err = ioutil.WriteFile(serviceFile, data, 0777)
+	err = ioutil.WriteFile(initServiceFile, data, 0777)
 	if err != nil {
 		return err
 	}
@@ -44,16 +105,33 @@ func (this *ServiceManager) Install(exePath string, args []string) error {
 	return nil
 }
 
-// 启动服务
-func (this *ServiceManager) Start() error {
-	return exec.Command("service", "teaweb", "start").Start()
-}
+// install systemd service
+func (this *ServiceManager) installSystemdService(systemd, exePath string, args []string) error {
+	desc := `[Unit]
+Description=TeaWeb Service
 
-// 删除服务
-func (this *ServiceManager) Uninstall() error {
-	f := files.NewFile(serviceFile)
-	if f.Exists() {
-		return f.Delete()
+[Service]
+Type=forking
+ExecStart=` + exePath + ` start
+ExecStop=` + exePath + ` stop
+ExecReload=` + exePath + ` reload
+
+[Install]
+WantedBy=multi-user.target`
+
+	// write file
+	err := ioutil.WriteFile(systemdServiceFile, []byte(desc), 0777)
+	if err != nil {
+		return err
 	}
-	return nil
+
+	// stop current systemd service if running
+	exec.Command(systemd, "stop", "teaweb.service")
+
+	// reload
+	exec.Command(systemd, "daemon-reload")
+
+	// enable
+	cmd := exec.Command(systemd, "enable", "teaweb.service")
+	return cmd.Run()
 }
