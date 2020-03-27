70d9d2bf83cf9dc00ebc8c7c25198b51a2a585f8
刘祥超 <iwind.liu@gmail.com>
2020-03-27 16:31:38 +0800

    [proxy]正向代理：可以记录请求和响应Body

--- teaproxy/request_forward.go
+++ teaproxy/request_forward.go
@@ -1,17 +1,51 @@
 package teaproxy
 
 import (
+	"bytes"
+	"github.com/TeaWeb/code/tealogs/accesslogs"
 	"github.com/TeaWeb/code/teautils"
 	"github.com/iwind/TeaGo/lists"
 	"io"
+	"io/ioutil"
 	"net"
 	"net/http"
+	"net/http/httputil"
 	"time"
 )
 
 // 正向代理
 func (this *Request) Forward(writer *ResponseWriter) error {
 	defer this.log()
+
+	// watch
+	if this.isWatching {
+		// 判断如果Content-Length过长，则截断
+		reqData, err := httputil.DumpRequest(this.raw, true)
+		if err == nil {
+			if len(reqData) > 100240 {
+				reqData = reqData[:100240]
+			}
+			this.requestData = reqData
+		}
+
+		writer.SetBodyCopying(true)
+	} else {
+		max := 512 * 1024 // 512K
+		if this.accessLog != nil && lists.ContainsInt(this.accessLog.Fields, accesslogs.AccessLogFieldRequestBody) {
+			body, err := ioutil.ReadAll(this.raw.Body)
+			if err == nil {
+				if len(body) > max {
+					this.requestData = body[:max]
+				} else {
+					this.requestData = body
+				}
+			}
+			this.raw.Body = ioutil.NopCloser(bytes.NewReader(body))
+		}
+		if this.accessLog != nil && lists.ContainsInt(this.accessLog.Fields, accesslogs.AccessLogFieldResponseBody) {
+			writer.SetBodyCopying(true)
+		}
+	}
 
 	if len(this.raw.URL.Scheme) == 0 {
 		this.rawScheme = "https"
